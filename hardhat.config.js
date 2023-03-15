c33d7f55900bb29251720a72762e55a142929d34
FranNav <frannavmart@gmail.com>
2023-03-15 18:04:09 +0000

    Add testnet network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  // networks: {
+  //   sepolia: {
+  //     url: `URL_SEPOLIA${process.env.INFURA_PROJECT_ID}`,
+  //     accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+  //   },
+  // },
 };
